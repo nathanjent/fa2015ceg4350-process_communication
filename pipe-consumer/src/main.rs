8da8b29c1c37b8726c29c8ced9d708a1441912bf
Nathan Jent <nathanjent@gmail.com>
2015-09-30 00:24:24 -0400

    Added travis.yml to test travis ci. Updated pipe. It compiles now.

--- pipe-consumer/src/main.rs
+++ pipe-consumer/src/main.rs
@@ -1,9 +1,21 @@
 use std::io;
+use std::io::LineWriter;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let mut data = String::new();
-    loop {
+    let mut data = String::new(); 
+    let f = File::create("consumer_in.txt")
+            .ok()
+            .expect("Failed to create file.");
+    let mut writer = LineWriter::new(f);
+    
+    for  i in (0..100).enumerate()  {
         io::stdin().read_line(&mut data).unwrap();
+        writer.write(data.as_bytes())
+                .ok()
+                .expect("Failed to write to file.");
+        
         println!("{}",data);
     }
 }
--- pipe-producer/src/main.rs
+++ pipe-producer/src/main.rs
@@ -1,19 +1,34 @@
 extern crate rand;
 
-use rand::Rng;
+use rand::distributions::{IndependentSample, Range};
 use std::process::{Command, Stdio};
-use std::io::{Write};
+use std::io::BufWriter;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let mut child = Command::new("pipe-consumer")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+    let f = File::create("producer_out.txt")
+            .ok()
+            .expect("Failed to create file.");
+    let r = Range::new(1, 1001);
+    let mut rng = rand::thread_rng();
+    {
+        let mut writer = BufWriter::new(f);
+        let mut child = Command::new("pipe-consumer")
+            .stdin(Stdio::piped())
+            .spawn()
+            .unwrap();
 
-    let child_in = child.stdin.as_mut().unwrap();
-    for  i in (0..100).enumerate() {
-        let mut x = rand::thread_rng().gen_range(1, 101);
-        child_in.write(&mut x.asByte()).unwrap();
+        let child_in = child.stdin.as_mut().unwrap();
+        let mut x;
+        for  i in (0..100).enumerate() {
+            x = r.ind_sample(&mut rng);
+            child_in.write(&[(x as u8)])
+                    .ok()
+                    .expect("Failed to write to child process.");
+            writer.write(&[(x as u8)])
+                    .ok()
+                    .expect("Failed to write to file.");
+        }
     }
 }
